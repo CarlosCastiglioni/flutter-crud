1d01739b30a34ef2ba0d06369f79b7df22542011
carlos <juniorcastiglioni@gmail.com>
2022-03-13 15:56:06 -0300

    criando testes para os componentes da home page e login page

---
+++ test/modules/login/login_page_test.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_crud/components/email_input.dart';
+import 'package:flutter_crud/components/password_input.dart';
+import 'package:flutter_crud/modules/login/login_store.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('Testando se a login page carrega todos os widgets necessários',
+      (tester) async {
+    final controller = LoginStore();
+    final _emailController = TextEditingController();
+    final _passwordController = TextEditingController();
+
+    await tester.pumpWidget(MediaQuery(
+      data: const MediaQueryData(),
+      child: Builder(builder: (context) {
+        return MaterialApp(
+          home: Scaffold(
+            body: Form(
+              child: Center(
+                child: SingleChildScrollView(
+                  padding: const EdgeInsets.symmetric(horizontal: 16),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      const Padding(
+                        padding: EdgeInsets.all(50.0),
+                        child: Center(
+                            child: Text(
+                          "Login",
+                          style: TextStyle(color: Colors.green, fontSize: 30),
+                        )),
+                      ),
+                      EmailInput(
+                        emailController: _emailController,
+                      ),
+                      PasswordInput(passwordController: _passwordController),
+                      ElevatedButton(
+                          onPressed: () async {
+                            print("Botão funcionou");
+                            await controller.login();
+                          },
+                          child: const Text("Entrar"))
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        );
+      }),
+    ));
+    expect(find.byType(Scaffold), findsOneWidget);
+    expect(find.byType(Form), findsOneWidget);
+    expect(find.byType(Center), findsNWidgets(2));
+    expect(find.byType(Padding), findsWidgets);
+    expect(find.byType(ElevatedButton), findsWidgets);
+    expect(find.byType(Text), findsNWidgets(4));
+    expect(find.text('Login'), findsOneWidget);
+
+    var button = find.text("Entrar");
+    expect(button, findsOneWidget);
+    await tester.tap(button);
+    await tester.pump();
+  });
+}
